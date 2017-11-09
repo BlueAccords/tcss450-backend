9d9269dfedb257324e54cc14285cdf4fde9ca972
BlueAccords <myoldemailname@gmail.com>
2017-11-08 22:16:06 -0800

    disabling db reset

--- server/models/index.js
+++ server/models/index.js
@@ -42,7 +42,6 @@ Object.keys(db).forEach(modelName => {
  * NOTE: This will cause the database to recreate tables every time the server is restarted.
  * This should be removed during production and changes to the database schema will need to be done via migrations
  */
-sequelize.sync({force: true});
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
